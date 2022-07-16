2336e6f818cb0258471938978ec579a3e1aeddde
Luiz Franzon <luizfranzon05@hotmail.com>
2022-07-16 18:12:42 -0300

    added working timer

---
+++ script.js
@@ -0,0 +1,116 @@
+let actualYear = new Date().getFullYear()
+
+const birthdayLuiz =   new Date(`aug 05, ${actualYear} 23:59:59`).getTime();
+const birthdayPedro =  new Date(`dec 31, ${actualYear} 00:00:00`).getTime();
+const birthdayDaniel = new Date(`aug 17, ${actualYear} 00:00:00`).getTime();
+const birthdayRyan =   new Date(`apr 17, ${actualYear} 00:00:00`).getTime();
+const birthdayPaulo =  new Date(`dec 25, ${actualYear} 00:00:00`).getTime();
+const birthdayBia =    new Date(`may 25, ${actualYear} 00:00:00`).getTime();
+const birthdayOsmar =  new Date(`aug 05, ${actualYear} 00:00:00`).getTime();
+
+function calcLuiz() {
+    var now = new Date().getTime();
+    let difference = birthdayLuiz - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+    if (difference < 0) {
+        timerLuiz.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerLuiz.innerText = "é hoje"
+    } else {
+        timerLuiz.innerText = `${days} dias`
+    }
+}
+
+function calcPedro() {
+    var now = new Date().getTime();
+    let difference = birthdayPedro - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+
+    if (difference < 0) {
+        timerPedro.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerPedro.innerText = "é hoje"
+    } else {
+        timerPedro.innerText = `${days} dias`
+    }
+}
+
+function calcDaniel() {
+    var now = new Date().getTime();
+    let difference = birthdayDaniel - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+
+    if (difference < 0) {
+        timerDaniel.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerDaniel.innerText = "é hoje"
+    } else {
+        timerDaniel.innerText = `${days} dias`
+    }
+}
+
+function calcRyan() {
+    var now = new Date().getTime();
+    let difference = birthdayRyan - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+
+    if (difference < 0) {
+        timerRyan.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerRyan.innerText = "é hoje"
+    } else {
+        timerRyan.innerText = `${days} dias`
+    }
+}
+
+function calcPaulo() {
+    var now = new Date().getTime();
+    let difference = birthdayPaulo - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+
+    if (difference < 0) {
+        timerPaulo.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerPaulo.innerText = "é hoje"
+    } else {
+        timerPaulo.innerText = `${days} dias`
+    }
+}
+
+function calcBia() {
+    var now = new Date().getTime();
+    let difference = birthdayBia - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+
+    if (difference < 0) {
+        timerBia.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerBia.innerText = "é hoje"
+    } else {
+        timerBia.innerText = `${days} dias`
+    }
+}
+
+function calcOsmar() {
+    var now = new Date().getTime();
+    let difference = birthdayOsmar - now 
+    var days = Math.floor((difference) / (1000 * 60 * 60 * 24));
+
+    if (difference < 0) {
+        timerOsmar.innerText = "Já passou"
+    } else if (difference > 0 && difference < 86400000) {
+        timerOsmar.innerText = "é hoje"
+    } else {
+        timerOsmar.innerText = `${days} dias`
+    }
+}
+
+var timeCalcInterval = setInterval(function () {
+    calcLuiz()
+    calcPedro()
+    calcDaniel()
+    calcRyan()
+    calcPaulo()
+    calcBia()
+    calcOsmar()
+}, 1000);
